a8fcd06223d40250287e10ffc5dc8278ad00e1f0
silja graf <silja.graf@stud.hslu.ch>
2026-08-18 10:04:42 +0200

    skizzenPortfolio

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Webportfolio_SiljaGraf/' : '/',
+  //base: command === 'build' ? '/Webportfolio_SiljaGraf/' : '/',
+  base: '/',
 }))
